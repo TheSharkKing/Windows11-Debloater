a2780d48f63fe6d0de7a92a3a873bd5092e40565
TheSharkKing <27yarberrycd23@gmail.com>
2025-04-08 21:39:00 -0500

    Added a .bat file so it helps run the file without a code editor

---
+++ run_debloater.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Change directory to the script's location
+cd /d c:\Users\27yar\Documents\GitHub\Windows11-Debloater\windows-debloater\src
+REM Run the Python script
+python main.py
+pause
