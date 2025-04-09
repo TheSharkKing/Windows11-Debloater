c9bdb7f86c9516f8f36ff144e9c54096cb370dd3
TheSharkKing <27yarberrycd23@gmail.com>
2025-04-08 21:46:10 -0500

    Fixed .bat

--- run_debloater.bat
+++ run_debloater.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Change directory to the script's location
-cd /d c:\Users\27yar\Documents\GitHub\Windows11-Debloater\windows-debloater\src
+cd /d "%~dp0windows-debloater\src"
 REM Run the Python script
 python main.py
 pause
